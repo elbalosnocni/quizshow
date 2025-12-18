53d32d093b653ec1c6f1c46d0885c445d2adcd3f
vtc <wuzhanggong@gmail.com>
2025-12-18 13:13:32 +0700

    Add configuration for quiz settings and API URL

---
+++ config.js
@@ -0,0 +1,9 @@
+const API_URL = "PASTE_WEB_APP_URL_HERE";
+
+const QUIZ_CONFIG = {
+  totalQuestions: 5,
+  duration: 600,
+  passScore: 0.7,
+  startTime: new Date("2025-12-18T08:00:00"),
+  endTime:   new Date("2025-12-20T17:00:00")
+};
